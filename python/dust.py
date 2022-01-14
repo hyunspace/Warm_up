1eeb3c6e89c092cb80607342be60e61e0369c42e
hyunspace <hyun.elina@gmail.com>
2022-01-14 12:23:57 +0900

    Jan 14 2022_morning

---
+++ python/dust.py
@@ -0,0 +1,10 @@
+dust = 70
+
+if dust > 150:
+    print('매우나쁨')
+elif 80 < dust <= 150:
+    print('나쁨')
+elif 30 < dust <= 80:
+    print('보통')
+else:
+    print('좋음')
---
+++ python/hello.py
@@ -0,0 +1,9 @@
+# '변수' 라는 개념
+dust = 60
+
+greeting = "Hello, World!"
+
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
